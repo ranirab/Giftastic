4c05b902a313020b54e82decf9f5610a767a4226
Rani Rab <rani@Ranis-MacBook-Pro.local>
2018-04-02 09:40:15 -0400

    dynamic button

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -6,9 +6,11 @@ var topicsArray = ["hummingbird", "goat", "rabbit"];
 // // 2. Your app should take the topics in this array and create buttons in your HTML.
 
 function createButtons() {
+    $(".buttons").empty();
     for (i = 0; i < topicsArray.length; i++) {
         var topicsBtn = $("<button>");
         topicsBtn.addClass("topicsButton");
+        
         topicsBtn.text(topicsArray[i]);
         $(".buttons").append(topicsBtn);
     }
@@ -24,31 +26,31 @@ $(document).on("click", ".topicsButton", function () {
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + inputGif + apiKey;
 
     $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
+        url: queryURL,
+        method: "GET"
+    })
         .then(function (response) {
+            
             var data = response.data;
             $("#gifs-div").empty();
             for (i = 0; i < data.length; i++) {
                 var gifImage = $("<img>").attr("src", data[i].images.fixed_height_still.url);
-                var gifRating = $("<rating>").text(data[i].rating);
+                gifImage.attr("data-animate", data[i].images.fixed_height_small.url);
+                gifImage.attr("data-still", data[i].images.fixed_height_still.url);
+                gifImage.attr("data-state", "still");
+                var gifRating = $("<p>").text(data[i].rating);
                 console.log(data[i].images.fixed_height_still.url);
-                $("#gifs-div").append(gifImage, gifRating);
+                $("#gifs-div").append(gifImage);
+                gifImage.append(gifRating);
             }
         });
 
 });
 
-$("#select-gif").on("click", function (event) {
-    event.preventDefault();
 
-    var inputGif = $("#gif.input").val().trim();
-    searchGifTopics(inputGif);
-});
 
 // // 4. When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
-$(".gif").on("click", function () {
+$(".topicsBtn").on("click", function () {
     var state = $(this).attr("data-state");
     if (state === "still") {
         $(this).attr("src", $(this).attr("data-animate"));
@@ -57,6 +59,15 @@ $(".gif").on("click", function () {
         $(this).attr("src", $(this).attr("data still"));
         $(this).attr("data-state", "still");
     };
+});
+
+
+$("#select-gif").on("click", function (event) {
+    event.preventDefault();
+console.log("clicked");
+    var inputGif = $("#gif-input").val().trim();
+    topicsArray.push(inputGif);
+    createButtons();
 });
 // // 5. Under every gif, display its rating (PG, G, so on).
 
